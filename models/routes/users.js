e90fabe2cb96d23c2346494b64006b39fb7c681b
Anton Gusev <gans-AD@yandex.ru>
2022-10-25 17:39:33 +0300

    feat: router users

---
+++ models/routes/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// возвращает информацию о пользователе (email и имя)
+router.get('/me',...);
+
+// обновляет информацию о пользователе (email и имя)
+router.patch('/me',...);
+
+module.exports = router;
