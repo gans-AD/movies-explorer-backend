aea244a87dd531dc9288f732a3260211cd230613
Anton Gusev <gans-AD@yandex.ru>
2022-10-26 08:33:36 +0300

    feat: логирование и обработка ошибок

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const { errorHandler } = require('./middlewares/errorHandler');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
+const NotFoundError = require('./utils/errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,7 +17,15 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 // подключаем логгер запросов
 app.use(requestLogger);
 
-app.use(errorLogger)
+app.use(errorLogger);
+
+// обработка несуществующего маршрута
+app.use('*', () => {
+  throw new NotFoundError('несуществующий маршрут');
+});
+
+// централизованная обработка ошибок
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`приложение запущено на порте ${PORT}`);
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+// централизованная обработка ошибок
+function errorHandler(err, req, res, next) {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
+  next();
+}
+
+module.exports = { errorHandler };
