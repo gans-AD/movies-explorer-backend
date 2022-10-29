b4622217fa57baa9d2a18340824a6faca267afd6
Anton Gusev <gans-AD@yandex.ru>
2022-10-29 18:34:01 +0300

    feat: валидация запроса с помощью celebrate

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getCurrentUser, editUser } = require('../controllers/users');
 
 // возвращает информацию о пользователе (email и имя)
 router.get('/me', getCurrentUser);
 
 // обновляет информацию о пользователе (email и имя)
-router.patch('/me', editUser);
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    }),
+  }),
+  editUser,
+);
 
 module.exports = router;
